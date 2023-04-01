dab91eca31acd96c0b09e77460c3db50307ccfea
Param Pragyan <parampragyan@gmail.com>
2023-04-01 23:10:41 +0530

    added a logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import "./Components.css"
+import logo from "../assets/logo.png"
 
 
 const NavBar = () => {
   return (
     <div className='nav-head bg-black text-white fixed w-[100%] z-[1000] '>
-        <div className="nav-bar w-[100%] flex justify-center items-center flex-row text-[4.5rem] font-[N-B-1] ">
+        <div className="nav-bar w-[100%] pl-[14.6rem] pr-[14.6rem] flex py-[.5rem]  items-center justify-between flex-row text-[4.5rem] font-[N-B-1] ">
+          <div className="logo"><img  className='logo-img h-[60px]' src={logo} alt="" /></div>
             <ul className='flex p-[2rem]'>
-                <li className='px-[1rem]'>Home</li>
-                <li className='px-[1rem]'>About</li>
-                <li className='px-[1rem]'>Works</li>
-                <li className='px-[1rem]'>Projects</li>
-                <li className='px-[1rem]'>Contact</li>
+                <li className='px-[1rem]'>HOME</li>
+                <li className='px-[1rem]'>ABOUT</li>
+                <li className='px-[1rem]'>WORKS</li>
+                <li className='px-[1rem]'>PROJECTS</li>
+                <li className='px-[1rem]'>CONTACT</li>
             </ul>
+            <div className="toggle">++++</div>
         </div>
     </div>
   )
--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -7,14 +7,24 @@ import github from "../../assets/github.svg"
 const Home = () => {
 
   const [position1, setPosition1] = useState(false);
+  const [position2, setPosition2] = useState(false);
+
 
   const changePosition1 = () => {
       if (window.scrollY >= 50) {
         setPosition1(true);
-      } else {
+      }  else {
        setPosition1(false);
       }
      };
+
+    //  const changePosition2 =() =>{
+    //   if (window.scrollY >= 150) {
+    //     setPosition2(true);
+    //   } else {
+    //    setPosition2(false);
+    //   }
+    //  }
 
      window.addEventListener("scroll", changePosition1);
 
@@ -30,11 +40,11 @@ const Home = () => {
         I build things for the{" "}
         <span className="web font-[N-B-1] text-[7rem] text-[#5ce1e6]">Web</span>
       </div>
-      <div className="bio text-[2rem] font-[Poppins] text-white font-[550]">
+      <div className="bio text-[1.8rem] font-[Poppins] text-white font-[550]">
         I'm a front-end developer who is passionate about making stunning,
         user-friendly websites.
       </div>
-      <button className=" text-white border-[3px] border-[#5ce1e6] bg-[#010006] rounded-[3rem] px-[4rem] py-[.5rem] mt-[3rem] check-btn text-[2.5rem] font-sans font-[500] ">
+      <button className=" btn-hero text-white border-[3px] border-[#5ce1e6] bg-[#010006] rounded-[3rem] px-[4rem] pb-[.5rem] mt-[3rem] check-btn text-[2.5rem] font-sans font-[400] ">
         Check out my Work!
       </button>
       <div className="w-full overflow-hidden" >
@@ -43,6 +53,9 @@ const Home = () => {
       
 
       <div className="right-strap h-[192.5vh]  bg-[#010006] w-[10rem] absolute right-[0rem] top-[80rem]"></div>
+      {/* <div className={ position1? "icon social-icons absolute top-[55rem] right-[19rem] ": position2? " icon social-icons absolute  top-[65rem] right-[30rem]": " icon social-icons absolute  top-[45rem] right-[8rem]"}>
+        <img src={github} height="60px" width="60px" className="icon border-[white] border-[2px] rounded-[50%] " alt="" />
+      </div> */}
       <div className={ position1? "icon social-icons absolute top-[55rem] right-[19rem] ": " icon social-icons absolute  top-[45rem] right-[8rem]"}>
         <img src={github} height="60px" width="60px" className="icon border-[white] border-[2px] rounded-[50%] " alt="" />
       </div>
