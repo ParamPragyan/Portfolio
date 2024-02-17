e378f259eb7b28e55376e26fb5f48084bde2a962
Param Pragyan <parampragyan@gmail.com>
2024-02-18 01:48:39 +0530

    minor changes

--- src/Sections/Projects/ProjectGrid.jsx
+++ src/Sections/Projects/ProjectGrid.jsx
@@ -13,8 +13,8 @@ const ProjectGrid = () => {
             key={idx}
             className="drop-project  flex p-[3rem] items-center justify-center hover:translate-y-[-2rem] transition-all duration-500"
           >
-            <div className="group border-white h-[22rem] duration-500 max-md:h-[28rem] w-full rounded-[5px] relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30  ">
-              <div className="h-[22rem] duration-500 max-md:h-[28rem]  w-fit">
+            <div className="group border-white h-[42rem] duration-500 max-md:h-[28rem] w-full rounded-[5px] relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30  ">
+              <div className="h-[42rem] duration-500 max-md:h-[28rem]  w-fit">
                 <img
                   className="h-full w-full object-top	 object-contain transition-transform duration-500 group-hover:scale-150 group-hover:translate-y-[20%]"
                   src={project.image}
@@ -22,7 +22,7 @@ const ProjectGrid = () => {
                 />{" "}
               </div>
               <div className="absolute inset-0 max-md: bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/30 group-hover:via-black/70 group-hover:to-black/80"></div>
-              <div className="absolute inset-0 max-md:inset-[0rem] flex translate-y-[60%] flex-col items-center justify-center px-0 text-center transition-all duration-500 group-hover:translate-y-0">
+              <div className="absolute inset-5 max-md:inset-[0rem] flex translate-y-[60%] flex-col items-center justify-center px-0 text-center transition-all duration-500 group-hover:translate-y-0">
                 <h1 className=" text-3xl p-3 max-md:p-4 max-md:text-5xl transition-all duration-500 rounded-t-2xl bg-white font-bold text-BLACK">
                   {project.tittle}
                 </h1>
